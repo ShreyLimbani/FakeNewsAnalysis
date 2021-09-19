994d100d590567f1b0bcb58aee51740aeeebade9
Shrey <shreylimbani@gmail.com>
2021-09-19 15:50:54 +0530

    Added Search Bar to Index

--- fakenews/urls.py
+++ fakenews/urls.py
@@ -5,7 +5,9 @@ from . import views
 urlpatterns = [
     path("",views.index, name="index"),
     path("index", views.index),
+    path("searchresults", views.searchresults, name="searchresults"),
     path("searchnews", views.searchnews, name="searchnews"),
+    path("searchnewsresults", views.searchnewsresults, name="searchnewsresults"),
     path("analytics", views.analytics, name="analytics"),
     path("about", views.about, name="about"),
 ]
--- fakenews/views.py
+++ fakenews/views.py
@@ -4,8 +4,15 @@ from django.shortcuts import redirect, render
 def index(request):
     return render(request, "fakenews/index.html")
 
+def searchresults(request):
+    search = request.GET['search']
+    return render(request, "fakenews/searchresults.html",context={'search':search})
+
 def searchnews(request):
     return render(request, "fakenews/searchnews.html")
+
+def searchnewsresults(request):
+    return render(request, "fakenews/searchnewsresults.html")
 
 def analytics(request):
     return render(request, "fakenews/analytics.html")
